e39f9fbaea85bf52d711035929257406a3f7c1ec
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-12 12:44:18 +0100

    init

---
+++ Submodule.cs
@@ -0,0 +1,10 @@
+﻿namespace Submodule
+{
+    public class Submodule
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Hello from submodule");
+        }
+    }
+}
